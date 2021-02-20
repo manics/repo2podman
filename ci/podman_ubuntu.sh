a6d0842620729b8f07ff440a1d8669d1f2251e1f
Simon Li <orpheus+devel@gmail.com>
2021-02-20 23:33:05 +0000

    Update Vagrantfile and setup script

--- ci/podman_ubuntu.sh
+++ ci/podman_ubuntu.sh
@@ -10,25 +10,16 @@ sudo apt-get update -qq
 # Travis defaults to --no-install-recommends
 sudo apt-get -qq -y install --install-recommends podman
 
-# systemd doesn't seem to work, use cgroupfs instead
-sudo sed -i.bak -re 's/cgroup_manager = .+/cgroup_manager = "cgroupfs"/' /usr/share/containers/libpod.conf
-
-# On ubuntu:bionic running podman as non-root defaults to using VFS instead of overlay as the fuse-overlayfs package isn't available:
-# https://github.com/containers/libpod/blob/master/docs/tutorials/rootless_tutorial.md#ensure-fuse-overlayfs-is-installed
-# VFS is extremely inefficient so use a custom binary built using
-# https://github.com/containers/fuse-overlayfs/tree/v0.7.6#static-build
-
-sudo curl -sSfL https://users.openmicroscopy.org.uk/~spli/podman/fuse-overlayfs-0.7.6 -o /usr/bin/fuse-overlayfs
-sudo chmod +x /usr/bin/fuse-overlayfs
-
 fuse-overlayfs --version
 slirp4netns --version
 podman info
 
 # If in vagrant:
-# apt-get install -qq -y python3-venv
+if [ -d /home/vagrant ]; then
+    apt-get install -qq -y python3-venv
+    rm -f /etc/resolv.conf
+    echo nameserver 1.1.1.1 > /etc/resolv.conf
+fi
 # python3 -mvenv ~/venv
 # . ~/venv/bin/activate
-# rm -f /etc/resolv.conf
-# echo nameserver 1.1.1.1 | sudo tee /etc/resolv.conf
-# pip install git+https://github.com/manics/repo2docker.git@abstractengine
+# pip install -r /repo2docker-podman/dev-requirements.txt git+https://github.com/manics/repo2docker.git@abstractengine
